799160f456510cdd184d74b5ccb0fa5e4e56e08f
Ebune Daniel Yahaya <ebunedy@gmail.com>
2022-03-28 13:12:55 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import Navbar from "./components/Navbar";
 import CTASectionOne from "./components/CTASectionOne";
 function App() {
   return (
-    <div className="App font-poppins">
+    <>
       <Navbar />
       <CTASectionOne />
-    </div>
+    </>
   );
 }
 
